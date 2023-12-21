f087a0ed1d3d21bfe8443a98e5258fd245d590d4
Juliana Chaves Palm <julianachavespalm@gmail.com>
2023-12-21 14:38:13 -0300

    Update audio volume and background color

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -34,7 +34,8 @@ function checkMatch() {
         if (openCards[0].innerHTML === openCards[1].innerHTML) {
             openCards[0].classList.add("boxMatch");
             openCards[1].classList.add("boxMatch");
-            playSound('correct');
+            playSound('correct').audio.volume = 0.5;
+            
         } else {
             openCards[0].classList.remove("boxOpen");
             openCards[1].classList.remove("boxOpen");
